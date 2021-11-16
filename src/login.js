376c85a71a97e29b07e88f0d42513ad139903c16
Glitch (calendar-version-2-3) <none>
2021-11-16 22:55:15 +0000

    👌🎪 Checkpoint ./src/login.js:23025355/649

---
+++ src/login.js
@@ -0,0 +1,20 @@
+function onSignIn(googleUser) {
+  // get user profile information
+  var profile = googleUser.getBasicProfile();
+  
+  console.log("ID: " + profile.getId());
+  console.log("Full Name: " + profile.getName());
+  console.log("Email: " + profile.getEmail());
+
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+  //window.location.href('https://www.google.com');
+  //document.location.href = "https://calendar-version-2-3.glitch.me/main.html";
+  //document.location.href = "main.html";
+}
+
+function signOut() {
+  gapi.auth2.getAuthInstance().signOut().then(function() {
+    console.log('user signed out')
+  })
+}
